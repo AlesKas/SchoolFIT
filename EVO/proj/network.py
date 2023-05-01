8e377d30f1f9a7d07bb1f893956e65f9ac67fb0e
AlesKas <ales.kasparek@email.cz>
2023-05-01 17:17:46 +0200

    lul

--- EVO/proj/network.py
+++ EVO/proj/network.py
@@ -5,25 +5,19 @@ import numpy as np
 class SortingNetwork():
     comparators = []
     seen = set()
-    max_pairs = 0
 
     def objective(self, comps):
         return len(comps)
 
     def genPair(self, m, n):
-        x, y = random.randint(m, n), random.randint(m, n)
-        while True:
-            self.seen.add((x, y))
-            yield (x, y)
-            x, y = random.randint(m, n), random.randint(m, n)
-            while (x, y) in self.seen or x == y:
-                x, y = random.randint(m, n), random.randint(m, n)
+        for i in range(m, n):
+            for j in range(m, n):
+                if i == j:
+                    continue
+                self.comparators.append((i, j))
 
     def __init__(self, arr) -> None:
-        self.max_pairs =  5 * len(arr)
-        gen = self.genPair(0, len(arr)-1)
-        for _ in range(4 * len(arr)):
-            self.comparators.append(next(gen))
+        self.genPair(0, len(arr))
         self.arr = arr
 
     def sort(self, comps):
@@ -45,13 +39,15 @@ class SortingNetwork():
         scores = []
         for i in range(n_iterations):
             selection = random.randint(0, len(self.arr)-1)
-            comps.pop(selection)
+            pop = comps.pop(selection)
             candidate = comps
             candidate_eval = self.objective(comps)
-            if candidate_eval < best_eval and self.sort(None) == self.sort(comps):
+            if candidate_eval < best_eval and sorted(self.arr) == self.sort(candidate):
                 best, best_eval = candidate, candidate_eval
                 scores.append(best_eval)
                 print('new best')
+            if sorted(self.arr) != self.sort(candidate):
+                comps.append(pop)
             diff = candidate_eval - curr_eval
             t = temp / float(i + 1)
             metropolis = np.exp(-diff / t)
