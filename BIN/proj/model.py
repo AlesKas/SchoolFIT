7b1df9fc19d18e54051c5187455a5ed6b1eaf9c3
AlesKas <ales.kasparek@email.cz>
2023-05-03 15:56:26 +0200

    LMAO

--- BIN/proj/model.py
+++ BIN/proj/model.py
@@ -3,33 +3,53 @@ import torch.nn as nn
 class MnistCNN(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.conv1 = nn.Sequential(         
-            nn.Conv2d(
+        self.conv1 = nn.Conv2d(
                 in_channels=1,              
                 out_channels=16,            
                 kernel_size=5,              
                 stride=1,                   
                 padding=2,                  
-            ),                              
-            nn.ReLU(),                      
-            nn.MaxPool2d(kernel_size=2),    
-        )
-        self.conv2 = nn.Sequential(         
-            nn.Conv2d(
+            )
+        self.conv2 = nn.Conv2d(
                 in_channels=16,
                 out_channels=32, 
                 kernel_size=5, 
                 stride=1, 
-                padding=2),     
-            nn.ReLU(),                      
-            nn.MaxPool2d(kernel_size=2),                
-        )
+                padding=2
+            )
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        # self.conv1 = nn.Sequential(         
+        #     nn.Conv2d(
+        #         in_channels=1,              
+        #         out_channels=16,            
+        #         kernel_size=5,              
+        #         stride=1,                   
+        #         padding=2,                  
+        #     ),                              
+        #     nn.ReLU(),                      
+        #     nn.MaxPool2d(kernel_size=2),    
+        # )
+        # self.conv2 = nn.Sequential(         
+        #     nn.Conv2d(
+        #         in_channels=16,
+        #         out_channels=32, 
+        #         kernel_size=5, 
+        #         stride=1, 
+        #         padding=2),     
+        #     nn.ReLU(),                      
+        #     nn.MaxPool2d(kernel_size=2),                
+        # )
         self.out = nn.Linear(32 * 7 * 7, 10)
         self.apply(self._init_weights)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
         x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
         x = x.view(x.size(0), -1)       
         output = self.out(x)
         return output
